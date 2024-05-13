b5ddcd3a501140ec616d2b95fc2876aca1aa0a3c
jabbathenut <grimesje@protonmail.com>
2024-05-12 20:22:47 -0700

    Modify luasnip dependency configuration

--- .config/nvim/lua/jabba/plugins/lsp/nvim-cmp.lua
+++ .config/nvim/lua/jabba/plugins/lsp/nvim-cmp.lua
@@ -5,11 +5,7 @@ return {
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
 		"hrsh7th/cmp-nvim-lsp-signature-help", -- source for function signatures
-		{
-			"L3MON4D3/LuaSnip",
-			version = "v2.*", -- follow latest version
-			build = "make install_jsregexp", -- optional installation
-		},
+		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip", -- for autocompletion
 		"rafamadriz/friendly-snippets", -- useful snippets
 		"onsails/lspkind.nvim", -- vs-code style pictograms
