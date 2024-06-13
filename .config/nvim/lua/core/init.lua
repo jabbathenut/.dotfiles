24f11bd735b219b37ed150465d0bd6937c197995
jabbathenut <grimesje@protonmail.com>
2024-06-13 12:42:44 -0700

    Revert tab/indent back to 2 spaces

--- .config/nvim/lua/core/init.lua
+++ .config/nvim/lua/core/init.lua
@@ -1,10 +1,7 @@
--- Set global variables
 vim.g.mapleader = " "
 
--- Require modules
 require("core.options")
 require("core.lazy")
 require("core.keymaps")
 
--- Set winbar
 vim.o.winbar = "%{%v:lua.require('core.winbar-config').set_winbar()%}"
--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -15,9 +15,9 @@ opt.guicursor =
 	"n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 -- Tabs and Indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.softtabstop = 4
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
 opt.expandtab = true
 opt.smartindent = true
 opt.autoindent = true
